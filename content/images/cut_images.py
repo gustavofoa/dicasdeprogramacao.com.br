2e2c1b9782cf14e5cf80fc4e19dbc4ecb648e046
Gustavo Furtado <gustavo.foa@gmail.com>
2017-04-03 18:39:46 -0300

    Cut destaque images

---
+++ content/images/cut_images.py
@@ -0,0 +1,15 @@
+from PIL import Image, ImageOps, ImageDraw
+import glob, os, math
+
+for infile in glob.glob("*/destaque.png"):
+    file, ext = os.path.splitext(infile)
+    img = Image.open(infile)
+
+    ImageOps.fit(img, (75, 75), method=Image.BICUBIC, centering=(0.5, 0.5)).save(file + '-75x75.png')
+    ImageOps.fit(img, (150, 150), method=Image.BICUBIC, centering=(0.5, 0.5)).save(file + '-150x150.png')
+    ImageOps.fit(img, (207, 207), method=Image.BICUBIC, centering=(0.5, 0.5)).save(file + '-207x207.png')
+    ImageOps.fit(img, (270, 270), method=Image.BICUBIC, centering=(0.5, 0.5)).save(file + '-270x270.png')
+    ImageOps.fit(img, (305, 175), method=Image.BICUBIC, centering=(0.5, 0.5)).save(file + '-305x175.png')
+    ImageOps.fit(img, (300, 225), method=Image.BICUBIC, centering=(0.5, 0.5)).save(file + '-300x225.png')
+    ImageOps.fit(img, (1024, 768), method=Image.BICUBIC, centering=(0.5, 0.5)).save(file + '-1024x768.png')
+    print('Cut image - ' + file)
