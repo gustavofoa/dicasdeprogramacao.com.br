e79410d5092f26647ef369c343f079c3ac1a8837
Gustavo Furtado de Oliveira Alves <gustavo.alves@mult-e.com.br>
2018-01-16 07:59:13 -0200

    Fix search result page

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Gustavo Furtado de Oliveira Alves'
 SITENAME = '{ Dicas de Programação }'
-SITEURL = 'http://localhost:1337/'
+SITEURL = 'http://localhost:1337'
 CONTACT_EMAIL = 'gustavo@dicasdeprogramacao.com.br'
 
 PATH = 'content'
