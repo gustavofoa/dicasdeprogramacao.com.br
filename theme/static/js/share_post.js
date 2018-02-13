7a948c23ffa0d911c1c852918b2164d00b87dc7c
Gustavo <gustavo.foa@gmail.com>
2018-02-13 13:10:56 -0200

    Setup share butons as floating.

--- theme/static/js/share_post.js
+++ theme/static/js/share_post.js
@@ -1,7 +1,7 @@
 var share_post_params = {"effect":"1","opacity":"0","top_distance":"220","opacity_intensity":"0.7"};
 
 window.onscroll = function scrollFunction() {
-    var scrollPos = document.body.scrollTop;
+    var scrollPos = window.scrollY;
 
     if (scrollPos > share_post_params.top_distance-20) {
         document.getElementById("share_post-box").style.position = "fixed";
