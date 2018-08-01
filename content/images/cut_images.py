afaa03d19887a082ea71b63c43601e50658f7cc9
Gustavo Furtado de Oliveira Alves <gustavo.foa@gmail.com>
2018-08-01 12:43:46 -0300

    New post and improve cut_image

--- content/images/cut_images.py
+++ content/images/cut_images.py
@@ -7,16 +7,28 @@ if len(sys.argv[1:]) == 1:
     folder = sys.argv[1].strip()
 print("Getting images from " + folder+".")
 
+def resize(size, img, filename):
+    img.thumbnail(size, resample=Image.ANTIALIAS)
+    background = Image.new('RGBA', size, (255, 255, 255))
+    background.paste(
+        img, (int((size[0] - img.size[0]) / 2), int((size[1] - img.size[1]) / 2))
+    )
+    background.save(filename)
+
 for infile in glob.glob(folder + "/destaque.png"):
     file, ext = os.path.splitext(infile)
     img = Image.open(infile)
 
-    ImageOps.fit(img, (75, 75), method=Image.BICUBIC, centering=(0.5, 0.5)).save(file + '-75x75.png')
-    ImageOps.fit(img, (150, 150), method=Image.BICUBIC, centering=(0.5, 0.5)).save(file + '-150x150.png')
-    ImageOps.fit(img, (207, 207), method=Image.BICUBIC, centering=(0.5, 0.5)).save(file + '-207x207.png')
-    ImageOps.fit(img, (270, 270), method=Image.BICUBIC, centering=(0.5, 0.5)).save(file + '-270x270.png')
-    ImageOps.fit(img, (305, 175), method=Image.BICUBIC, centering=(0.5, 0.5)).save(file + '-305x175.png')
-    ImageOps.fit(img, (300, 225), method=Image.BICUBIC, centering=(0.5, 0.5)).save(file + '-300x225.png')
-    ImageOps.fit(img, (600, 315), method=Image.BICUBIC, centering=(0.5, 0.5)).save(file + '-600x315.png')
-    ImageOps.fit(img, (1024, 768), method=Image.BICUBIC, centering=(0.5, 0.5)).save(file + '-1024x768.png')
+    img.thumbnail((1024, 768), resample=Image.BICUBIC)
+    img.save(file + '-1024x768.png')
+    resize((1024,768), img, file + "-1024x768.png")
+    resize((600,315), img, file + "-600x315.png")
+    resize((300,225), img, file + "-300x225.png")
+    resize((305,175), img, file + "-305x175.png")
+    resize((270,270), img, file + "-270x270.png")
+    resize((207,207), img, file + "-207x207.png")
+    resize((150,150), img, file + "-150x150.png")
+    resize((75,75), img, file + "-75x75.png")
+    
+    
     print('Cut image - ' + file)
